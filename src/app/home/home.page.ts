332b519aacfc16890cd311daf7f45c7282794cfc
Leandro <leandro.sorbo@gmail.com>
2020-09-24 17:17:10 -0300

    Lista de tarefas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+type Task = {
+  name: string;
+  completed: boolean;
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public tasks: Task[] =[
+    { name: "Learn Ionic", completed: false },
+    { name: "Buy Milk", completed: true },
+  ];
+  public filteredTasks = this.tasks.slice();
+  public newTaskName = "";
+  public percent = 0;
+  public selectedSegment: "all" | "complete" | "incomplete" = "all";
+
+  public addTask(){
+    this.tasks.push({
+      name: this.newTaskName,
+      completed: false
+    });
+    this.newTaskName = "";
+    this.calculatePercent();
+  };
+
+    public remove(toRemove: Task){
+    this.tasks = this.tasks.filter(function (task) {
+      return task.name !== toRemove.name;
+    });
+    this.segmentChanged();
+    this.calculatePercent();
+    }
+  
+    public calculatePercent(){
+      const completedTasks = this.tasks.filter(function(task){
+        return task.completed;
+      });
+      this.percent = completedTasks.length / this.tasks.length;
+    }
+
+       public segmentChanged(){
+         if(this.selectedSegment === "all"){
+           this.filteredTasks = this.tasks.slice();
+         } else if (this.selectedSegment === "complete"){
+           this.filteredTasks = this.tasks.filter(function (task){
+             return task.completed;
+           });
+         } else {
+           this.filteredTasks = this.tasks.filter(function (task) {
+             return !task.completed;
+           });
+         }      
+    }
+
+
+  constructor() {
+    this.calculatePercent();
+  }
 
 }
