3c3e8797adc4dac2227c5645af31487818c5f468
Leandro <leandro.sorbo@gmail.com>
2020-09-25 15:35:50 -0300

    conta de banco

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 
+type Transaction = {
+  amount: number;
+  date: Date;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public selectedAmount = 0;
+  public totalAmount = 0;
+  public transactionLog: Transaction[] = [];
+
+  public makeTransaction() {
+    this.totalAmount += this.selectedAmount;
+    this.transactionLog.unshift({
+      amount: this.selectedAmount,
+      date: new Date()
+    })
+
+    this.selectedAmount = 0;
+  }
 
   constructor() {}
 
