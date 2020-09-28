eb8a450fb5829e6c1d442d55e8213f8dbcf51bb1
Leandro <leandro.sorbo@gmail.com>
2020-09-28 20:26:27 -0300

    app deposito

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public currentValue = 0;
+  public maxValue = 0;
+  public selectedCash = 0;
+
+  public paymentMethod = "debit";
+
+  public jonSnow = false;
+
+  public increment() {
+    this.currentValue += this.selectedCash;
+    this.selectedCash = 0;
+    if (this.currentValue > this.maxValue) {
+      this.maxValue = this.currentValue;
+    }
+  }
+
+  public decrement() {
+    this.currentValue -= this.selectedCash;
+    this.selectedCash = 0;
+    if (this.currentValue < 0) {
+      this.currentValue = 0;
+    }
+  }
+
+  public reset() {
+    this.currentValue = 0;
+  }
 
 }
