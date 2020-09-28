d4fc77c21038eb4b087f9d3b1a986d72e20eedef
Leandro <leandro.sorbo@gmail.com>
2020-09-28 20:18:09 -0300

    app contador

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public currentValue = 0;
+  public maxValue = 0;
 
+  public increment() {
+    this.currentValue++;
+    if (this.currentValue > this.maxValue) {
+      this.maxValue = this.currentValue;
+    }
+  }
+
+  public decrement() {
+    if (this.currentValue > 0) {
+      this.currentValue--;  
+    }
+  }
+
+  public reset() {
+    this.currentValue = 0;
+  }
 }
