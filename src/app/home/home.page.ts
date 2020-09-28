959ec8635f98e452326fb4754a466b3de064a8c9
Leandro <leandro.sorbo@gmail.com>
2020-09-28 20:28:37 -0300

    app contador duplo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  public currentValue = 0;
+  
+  public increment() {
+    this.currentValue++;
+    
+  }
 }
