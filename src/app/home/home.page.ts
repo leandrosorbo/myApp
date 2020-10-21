55d0307286f7b531c07759460c75842dd1453471
Leandro <leandro.sorbo@gmail.com>
2020-10-21 11:46:39 -0300

    atualização

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, Platform } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,57 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  selectedDateString: string = new Date().toISOString();
+  minDate: string = new Date().toISOString();
+  maxDate: string = new Date().toISOString();
 
-}
+    constructor(public actionSheetController: ActionSheetController, private platform: Platform) {
+this.platform.ready().then(() => {
+let date: Date = new Date();
+date.setDate(date.getDate() - 1);
+this.minDate = date.toISOString();
+
+date = new Date();
+date.setDate(date.getDate() + 365);
+this.maxDate = date.toISOString();
+
+
+});
+
+    }
+
+
+    async selectLocation() {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Locais',
+        cssClass: 'my-custom-class',
+        buttons: [{
+          text: 'Shopping Aricanduva',
+          icon: 'location',
+          handler: () => {
+            console.log('Shopping Aricanduva');
+          }
+        }, {
+          text: 'Shopping Mooca',
+          icon: 'location',
+          handler: () => {
+            console.log('Shopping Mooca');
+          }
+        }, {
+          text: 'Shopping Pátio Higienólis',
+          icon: 'location',
+          handler: () => {
+            console.log('Shopping Pátio Higienólopis');
+          }
+        }, {
+          text: 'Cancelar',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        }]
+      });
+      await actionSheet.present();
+    }
+  }
